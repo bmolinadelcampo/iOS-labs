3e7c78018ee4418e08c169cd3d0c8c39f3d27da5
Belén Molina del Campo <bmolinadelcampo@gmail.com>
2016-03-22 16:30:40 +0000

    Add dependent picker to Pickers.

--- Pickers/Pickers/Pickers/ThirdViewController.m
+++ Pickers/Pickers/Pickers/ThirdViewController.m
@@ -7,31 +7,83 @@
 //
 
 #import "ThirdViewController.h"
+#define kShowsComponent 0
+#define kCharactersComponent 1
 
 @interface ThirdViewController ()
+@property (strong, nonatomic) NSDictionary *showsCharacters;
+@property (strong, nonatomic) NSArray *shows;
+@property (strong, nonatomic) NSArray *characters;
+@property (weak, nonatomic) IBOutlet UIPickerView *myDependentPicker;
 
+- (IBAction)myButton:(id)sender;
 @end
 
 @implementation ThirdViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSBundle *myBundle = [NSBundle mainBundle];
+    NSString *plistPath = [myBundle pathForResource:@"shows-characters" ofType:@"plist"];
+    self.showsCharacters = [[NSDictionary alloc] initWithContentsOfFile:plistPath];
+    
+    
+    NSArray *allKeys = [self.showsCharacters allKeys];
+    NSArray *sortedKeys = [allKeys sortedArrayUsingSelector:@selector(compare:)];
+    self.shows = sortedKeys;
+    
+    NSString *selectedShow = self.shows[0];
+    self.characters = [self.showsCharacters objectForKey:selectedShow];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 2;
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    if (component == kShowsComponent) {
+        return [self.shows count];
+    }else{
+        return [self.characters count];
+    }
 }
-*/
 
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    if (component == kShowsComponent) {
+        return self.shows[row];
+    }else{
+        return self.characters[row];
+    }
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    if (component == kShowsComponent) {
+        NSString *selectedShow = self.shows[row];
+        self.characters = [self.showsCharacters objectForKey:selectedShow];
+        [self.myDependentPicker selectRow:0 inComponent:kCharactersComponent animated:YES];
+        [self.myDependentPicker reloadComponent:kCharactersComponent];
+    }
+}
+
+- (IBAction)myButton:(id)sender {
+    NSInteger showRow = [self.myDependentPicker selectedRowInComponent:kShowsComponent];
+    NSInteger characterRow = [self.myDependentPicker selectedRowInComponent:kCharactersComponent];
+    NSString *show = self.shows[showRow];
+    NSString *character = self.characters[characterRow];
+    NSString *title = [NSString stringWithFormat:@"You selected the show %@.", show];
+    NSString *message = [NSString stringWithFormat:@"%@ is in %@", character, show];
+    
+    UIAlertController *myAlert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action){
+        NSLog(@"OK button was pressed");
+    }];
+    
+    [myAlert addAction:ok];
+    [self presentViewController:myAlert animated:YES completion:nil];
+}
 @end
