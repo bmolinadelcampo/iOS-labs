c42737306820126d9cd499094b7577b858639331
Belén Molina del Campo <bmolinadelcampo@gmail.com>
2016-03-23 14:48:02 +0000

    Add Static Cells (Lab 21).

---
+++ Static Cells/Static Cells/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Static Cells
+//
+//  Created by Belén Molina del Campo on 23/03/2016.
+//  Copyright © 2016 Belén Molina del Campo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
