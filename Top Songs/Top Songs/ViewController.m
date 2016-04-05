74f9018be439bf5fd260256788e903b17d8274ea
Belén Molina del Campo <bmolinadelcampo@gmail.com>
2016-04-05 10:23:59 +0100

    Add steps 1 and 2 of Top Songs (Lab 29).

---
+++ Top Songs/Top Songs/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Top Songs
+//
+//  Created by Belén Molina del Campo on 05/04/2016.
+//  Copyright © 2016 Belén Molina del Campo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () 
+@property (strong, nonatomic) NSMutableArray *songsArray;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Top Songs/Top Songs/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Top Songs
+//
+//  Created by Belén Molina del Campo on 05/04/2016.
+//  Copyright © 2016 Belén Molina del Campo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
